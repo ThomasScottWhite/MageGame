ed2cef22e4c942fae7521e68bb4ff9bc1ebd41db
ThomasScottWhite <95893088+ThomasScottWhite@users.noreply.github.com>
2022-09-02 11:35:44 -0500

    Added Attack Logic

--- Scripts/Enemies/Zombie.gd
+++ Scripts/Enemies/Zombie.gd
@@ -3,7 +3,13 @@ extends AnimatedSprite
 
 # Declare member variables here. Examples:
 var health = 50
-var speed = 50
+var baseSpeed = 50
+var currentSpeed = baseSpeed
+var zombie_melee_range = 5
+var zombie_damage = 10
+var attackCooldown = 1.0
+var currentAttackCooldown = 0
+
 onready var player = get_node("../Player")
 
 # Called when the node enters the scene tree for the first time.
@@ -18,11 +24,21 @@ func takedamage(damage_position,damage_range,amount):
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	currentAttackCooldown -= delta
 	if player.position.x >= self.position.x:
-		self.position.x += speed * delta
+		self.position.x += currentSpeed * delta
 	if player.position.x <= self.position.x:
-		self.position.x -= speed * delta
+		self.position.x -= currentSpeed * delta
 	if player.position.y >= self.position.y:
-		self.position.y += speed * delta
+		self.position.y += currentSpeed * delta
 	if player.position.y <= self.position.y:
-		self.position.y -= speed * delta
+		self.position.y -= currentSpeed * delta
+		
+	if currentAttackCooldown >= 0:
+		currentAttackCooldown -= delta
+		currentSpeed = 0
+	else:
+		currentSpeed = baseSpeed
+	if player.position.distance_to(self.position) <= zombie_melee_range and currentAttackCooldown <= 0:
+		currentAttackCooldown = attackCooldown
+		event_bus.emit_signal("player_damage", self.position,zombie_melee_range,zombie_damage)
